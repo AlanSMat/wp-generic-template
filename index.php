c23e083f50ff4e58880e188146003040c018ba6c
AlanSMat <steve@asmather.com>
2023-10-29 12:15:43 +1100

    Added header footer

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
+get_header();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ASM Digital</title>
-</head>
-<body>
-    
-</body>
-</html>
+    <div class="content">
+        Content
+    </div>
+<?php
+get_footer();
